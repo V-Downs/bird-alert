6ce274a1880aa803ee12025e55a99ee5d728ddc5
Declan lacey <declan.lacey@hotmail.com>
2024-09-21 16:52:39 -0500

    Fixed bug

--- src/app/RescueDetails/RescueDetails.tsx
+++ src/app/RescueDetails/RescueDetails.tsx
@@ -96,7 +96,7 @@ export default function RescueDetails({ rescue, onBack, selectedRescue, setSelec
 
     const handleSubmit = async (e: React.FormEvent) => {
 
-        const fields = { CurrentVolunteer: localRescuerName}
+        const fields = {CurrentVolunteer: localRescuerName, VolunteerStatus: "In Route"}
         try {
             const updatedRecords = await base('Bird Alerts').update([
                 {
@@ -233,7 +233,7 @@ export default function RescueDetails({ rescue, onBack, selectedRescue, setSelec
                 <div className="space-y-4">
                     Technical Skills:
                     <ul>
-                        {rescue.skills?.map((skill) => <li>{skill}</li>)}
+                        {rescue.skills?.map((skill, index) => <li key={index}>{skill}</li>)}
                     </ul>
                 </div>
 
