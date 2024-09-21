efa2c4f8134d64fe69b964ba995026a4da8c05a4
Travis Sanderson <travis.sanderson@gmail.com>
2024-09-21 15:05:59 -0500

    Allow changing the filter for bird alert list

--- src/app/BirdAlertList/page.tsx
+++ src/app/BirdAlertList/page.tsx
@@ -1,12 +1,21 @@
 'use client'
 
-import { MapPinIcon, BirdIcon, TruckIcon, HomeIcon, CheckCircleIcon, MoreHorizontalIcon, UserIcon, ListIcon, MapIcon, ArrowLeftIcon, NavigationIcon, ChevronUpIcon, ChevronDownIcon, ShieldIcon, FilterIcon, XIcon } from 'lucide-react'
+import {
+    MapPinIcon,
+    TruckIcon,
+    HomeIcon,
+    ChevronsUpDown
+} from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle, CardFooter } from '@/components/ui/card'
 import { useEffect, useState } from 'react'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import RescueDetails from '../RescueDetails/RescueDetails'
 import Airtable from 'airtable'
+import {Checkbox} from "@/components/ui/checkbox";
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command'
+import {cn} from "@/lib/utils";
 
 type RescueStatus = 'Pending' | 'In Route' | 'Rescued' | 'Delivered'
 
@@ -19,19 +28,21 @@ interface Bird {
   rescuerName: string
 }
 
-
 export default function BirdAlertList() {
     const [location, setLocation] = useState<string>('Des Moines, IA')
     const [birdRescues, setBirdRescues] = useState<Bird[]>([])
     const [selectedRescue, setSelectedRescue] = useState<Bird | null>(null)
     const [activeView, setActiveView] = useState<'list' | 'admin'>('list')
-    const [selectedStatuses, setSelectedStatuses] = useState<RescueStatus[]>(['Pending', 'In Route', 'Rescued', 'Delivered'])
+    const allStatuses = ['Pending', 'In Route', 'Rescued', 'Delivered'] as RescueStatus[];
+    const [value, setValue] = useState(new Set<RescueStatus>(['Pending', 'In Route', 'Rescued']))
     // const [selectedBirdTypes, setSelectedBirdTypes] = useState<BirdType[]>(['Songbird', 'Raptor', 'Waterfowl', 'Shorebird', 'Other'])
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
     const [showAcceptForm, setShowAcceptForm] = useState(false)
     const [rescuerName, setRescuerName] = useState('')
     const [rescuerPhone, setRescuerPhone] = useState('')
+
+    const [isFilterOpen, setIsFilterIsFilterOpen] = useState(false)
 
     const airtable = new Airtable({ apiKey: process.env.NEXT_PUBLIC_AIRTABLE_ACCESS_TOKEN })
     const base = airtable.base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID!)
@@ -66,20 +77,19 @@ export default function BirdAlertList() {
         }
         setIsLoading(false)
       }
-    
+
     console.log(birdRescues)
 
     useEffect(() => {
     fetchBirdRescues()
     }, [])
-    
 
     return (
         <div className="space-y-4">
       <Card className="bg-fill bg-center text-white rounded-none" style={{ backgroundImage: "url('../images/birds.jpg')" }}>
-       
+
           <CardTitle className="text-2xl font-bold px-8 pt-12 pb-2">Iowa Bird Rehabilitation</CardTitle>
-        
+
         <CardContent>
           <div className="flex items-center text-sm text-stone-300 pb-6">
             <MapPinIcon className="mr-2 h-4 w-4" />
@@ -93,10 +103,60 @@ export default function BirdAlertList() {
       ) : (
         <Card className="bg-white shadow-lg rounded-lg overflow-hidden">
           <CardHeader className="bg-stone-100 border-b border-stone-200">
-            <CardTitle className="text-lg md:text-xl font-semibold text-stone-800">Available Rescues</CardTitle>
+            <CardTitle className="text-lg md:text-xl font-semibold text-stone-800">
+                Available Rescues
+                <Popover open={isFilterOpen} onOpenChange={setIsFilterIsFilterOpen}>
+                    <PopoverTrigger asChild>
+                        <Button
+                            variant="outline"
+                            role="combobox"
+                            aria-expanded={isFilterOpen}
+                            className="w-[200px] justify-between"
+                        >
+                            {value
+                                ? Array.from(value).join(',')
+                                : "Select framework..."}
+                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                        <Command>
+                            <CommandInput placeholder="Select statuses..." />
+                            <CommandList>
+                                <CommandEmpty>No statuses found.</CommandEmpty>
+                                <CommandGroup>
+                                    {allStatuses.map((framework) => (
+                                        <CommandItem
+                                            onSelect={(currentValue) => {
+                                                    const newValue = new Set(value)
+                                                    if (newValue.has(framework)) {
+                                                        newValue.delete(framework)
+                                                    } else {
+                                                        newValue.add(framework)
+                                                    }
+                                                    setValue(newValue)
+                                            }}
+                                        >
+                                            <Checkbox
+                                                checked={value.has(framework)}
+                                                className={cn(
+                                                    "mr-2 h-4 w-4",
+                                                )}
+                                            />
+                                            {framework}
+                                        </CommandItem>
+                                    ))}
+                                </CommandGroup>
+                            <CommandItem>
+                                <Button onClick={() => setIsFilterIsFilterOpen(false)}>Done</Button>
+                            </CommandItem>
+                            </CommandList>
+                        </Command>
+                    </PopoverContent>
+                </Popover>
+            </CardTitle>
           </CardHeader>
           <CardContent className="p-4">
-            {/* <FilterOptions /> */}
             {isLoading ? (
               <div className="flex justify-center items-center h-32">
                 <div className="w-8 h-8 border-t-2 border-lime-500 rounded-full animate-spin" />
@@ -107,14 +167,16 @@ export default function BirdAlertList() {
               </div>
             ) : (
               <div className="grid grid-cols-1 gap-4">
-                {birdRescues.filter(bird => 
-                  selectedStatuses.includes(bird.status) && bird.status !== "Delivered"
-                ).map(rescue => (
+                {birdRescues.filter(bird =>
+                  value.has(bird.status)
+                ).sort((a, b) => {
+                    // Show pending rescues first
+                    return a.status == 'Pending' ? -1 : 1
+                }).map(rescue => (
                   <Card key={rescue.id} className="overflow-hidden">
                     <CardHeader className="p-4">
                       <div className="flex justify-between items-center">
                         <div className="flex items-center space-x-2">
-                          {/* <span className="text-2xl">{getBirdTypeIcon(rescue.birdType)}</span> */}
                           <CardTitle className="text-lg font-semibold">{rescue.species}</CardTitle>
                         </div>
                         <Badge variant="secondary" className={`${getStatusColor(rescue.status)} text-white h-10`}>
@@ -140,7 +202,7 @@ export default function BirdAlertList() {
                       </div>
                     </CardContent>
                     <CardFooter className="bg-stone-50 p-4">
-                      <Button 
+                      <Button
                         className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input shadow-sm hover:text-accent-foreground h-9 px-4 py-2 w-full bg-white hover:bg-stone-50 transition-colors duration-200 ease-in-out text-black"
                         onClick={() => setSelectedRescue(rescue)}
                       >
